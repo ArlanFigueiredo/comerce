5ca834dded33823fcd5cecc77ba55bc79c851ce6
Arlan Figueiredo <estudosarlan@gmail.com>
2024-03-09 14:01:30 -0300

    created route of order

--- src/http/routes/routes.ts
+++ src/http/routes/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from 'fastify'
 import { register } from '../controller/user/register'
 import { registerAdm } from '../controller/adm/register'
 import { registerProduct } from '../controller/product/register'
+import { registerOrder } from '../controller/order/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/adm', registerAdm)
   app.post('/products', registerProduct)
+  app.post('/order/:product_id/:user_id', registerOrder)
 }
